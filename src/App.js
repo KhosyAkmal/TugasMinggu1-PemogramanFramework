75aa99201c5e3190b45edd9b64dec020825cb57e
KhosyAkmal <akmalkhosy@gmail.com>
2020-02-13 00:59:28 +0700

    Change logo to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logo.svg'
 import './App.css';
 import './index.css';
 import {
@@ -14,34 +15,40 @@ import About from './views/About';
 
 function App() {
   return (
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          Tugas Minggu 1
+    <div>
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">
+            Tugas Minggu 1
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/home">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <div>
-        <Switch>
-          <Route path="/home" exact component={Home}/>
-          <Route path="/about" exact component={About}/>
-        </Switch>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+        <header className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
       </div>
-    </Router>
+        <div>
+          <Switch>
+            <Route path="/home" exact component={Home} />
+            <Route path="/about" exact component={About} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg'
 import '../App.css';
 import Table from 'react-bootstrap/Table'
 
@@ -7,14 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div className="container">
-                <div>
-                    <header className="App">
-                        <img src={logo} className="App-logo" alt="logo" />
-                    </header>
-                </div>
-
                 <Table striped bordered hover size="sm" align="center" variant="light" >
-
                     <thead>
                         <tr>
                             <th>No</th>
